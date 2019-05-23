43c5c84ab4d7a088954955179a13d2410c6a9e3e
BokalinaR <bokanar@gmail.com>
2019-05-23 23:44:41 +0200

    Sredjeno osvetljenje i dodatne izmene

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <GL/glut.h>
-#include <sys/time.h>
-#include <time.h>
-#include <string.h>
+/* Student: Bojana Ristanovic */
+/* Indeks: 45/2016 */
+/* Predmet: Racunarska grafika */
+
 
 /***** NAPOMENA ****/
 /*
@@ -14,6 +11,14 @@
  * 3. pucanje implementirano takodje zahvaljujuci koleginici Andjelki Milovanovic
  * 
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <GL/glut.h>
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
 
 /* Dimenzije prozora */
 static int window_width, window_height;
@@ -27,18 +32,18 @@ static void on_mouse_motion(int x, int y);
 void material_and_light();
 
 /* Ugao rotacije oko y ose za pravac kamere*/
-float angleY = 0.0;
+float angleY;
 /* Ugao rotacije oko x ose za pravac kamere*/
-float angleX = 0.0;
+float angleX;
 /* Vektor koji predstavlja pravac kamere */
-float kx = 0.0f;
-float ky = 0.0f;
-float kz = -1.0f;
+float kx;
+float ky;
+float kz;
 
 /* Pozicija kamere */
-float x = 5.0f;
-float y = 0.0f;
-float z = 9.0f;
+float x;
+float y;
+float z;
 
 /* Pozicija misa*/
 GLfloat lastX;
@@ -46,10 +51,10 @@ GLfloat lastY;
 /* Osetljivost misa da ne leti po ekranu*/
 float sensitivity = 0.2;
 
-/* Tajemer za odbrojavanje do kraja igre */
-int game_over = 0;
-GLfloat diff_time;
+/* Odbrojavanje do kraja igre */
 GLfloat game_time = 0;
+int game_over;
+GLfloat diff_time;
 GLfloat beginTime;
 char disp_time[1000];
 int timing;
@@ -57,24 +62,24 @@ int timing;
 static int timer_active;
 
 /* Iscrtavanje kocki */
-float positionx[17];
-float positionz[17];
-float positiony[17];
+float positionx[25];
+float positionz[25];
+float positiony[25];
 
 
 /* Tajmer za ispucavanje kuglice */
-float t = 0;
-static int move_ball = 0;
+float t;
+static int move_ball;
 /* Pozicija kuglice */
 float x_ball; 
 float y_ball; 
 float z_ball; 
 /* Vektor pravca kuglice */
-float bx = 0.0f;
-float by = 0.0f;
-float bz = -1.0f;
+float bx;
+float by;
+float bz;
 /* brzina kuglice */
-float v = 7.0f;
+float v;
 /* Radijus kuglice */
 float radius = 0.05f;
 
@@ -82,7 +87,7 @@ float radius = 0.05f;
 static int ind = 0;
 
 /* Broj osvojenih bodova */
-int score = 0;
+int score;
 char disp_score[256];
 
 static void draw_coordinate_system();
@@ -92,12 +97,12 @@ void cube();
 static void on_time(int value);
 void drawBitmapText();
 void drawScore();
+void drawPointer(); 
 void shoot();
 static void on_mouse(int button, int state, int x, int y);
 void moving_ball(int value);
 
 void init_lights();
-
 void initialize();
 
 int main(int argc, char **argv)
@@ -111,9 +116,7 @@ int main(int argc, char **argv)
 	glutInitWindowSize(750, 650);
 	glutInitWindowPosition(350, 100);
 	glutCreateWindow("Box shooter");
-	
-	glutWarpPointer(glutGet(GLUT_SCREEN_WIDTH)/2, glutGet(GLUT_SCREEN_HEIGHT)/2);
-	
+		
 	/* Registruju se callback funkcije. */
 	glutKeyboardFunc(on_keyboard);
 	glutReshapeFunc(on_reshape);
@@ -136,46 +139,73 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void initialize()
+
+static void on_reshape(int width, int height)
 {
+	/* Pamte se sirina i visina prozora. */
+	window_width = width;
+	window_height = height;
 	
-	/* Ugao rotacije oko y ose za pravac kamere*/
-	angleY = 0.0;
-	/* Ugao rotacije oko x ose za pravac kamere*/
-	angleX = 0.0;
+	/* Podesava se viewport. */
+	glViewport(0, 0, window_width, window_height);
+}
+
+static void on_display(void)
+{
+	/* Brise se prethodni sadrzaj prozora. */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+	/* Podesava se projekcija. */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(
+		60,
+		1,
+		0.0,
+		10.0
+	);
+	/* Namestanje tacke pogleda kamere*/
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(x, 1.0f, z, 
+		  x+kx, 1.0f+ky, z+kz,
+	   0.0f, 1.0f, 0.0f
+	);
 	
-	/* Vektor koji predstavlja pravac kamere */
-	kx = 0.0f;
-	ky = 0.0f;
-	kz = -1.0f;
+	/* Ukljucivanje osvetljenja */
+	init_lights();
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_COLOR_MATERIAL);
 	
-	/* Pozicija kamere */
-	x = 5.0f;
-	y = 0.0f;
-	z = 9.0f;
+	glDisable(GL_DEPTH_TEST);
 	
-	/* Tajmer za odbrojavanje do kraja igre */
-	game_over = 0;
-	game_time = 0;
-	/* Fleg koji odredjuje stanje tajmera*/
-	timer_active = 0;
+	draw_coordinate_system();
+	draw_box();
+	cube();
 	
-	/* Tajmer za ispucavanje kuglice */
-	t = 0;
-	move_ball = 0;
-	/* Vektor pravca kuglice */
-	bx = 0.0f;
-	by = 0.0f;
-	bz = -1.0f;
-	/* brzina kuglice */
-	v = 7.0f;
+	/* Incijalizuje poziciju kuglice za ispaljivanje*/
+	x_ball = x;
+	y_ball =y;
+	z_ball = z;
+	shoot();
 	
-	/* Indikator da li je tajmer bio aktivan */
-	ind = 0;
+	/* Iscrtava nisan */
+	drawPointer(); 
 	
-	/* Broj osvojenih bodova */
-	score = 0;
+	/* Ispisivanje vremena */
+	drawBitmapText();
 	
+	/* Ispisivanje rezultata*/
+	int score1  = sprintf(disp_score,"%d", score);
+	if(score1 < 0)
+		exit(1);
+	if(!timer_active && ind == 1){
+		drawScore();
+	}
+	
+	glutPostRedisplay();
+	/* Slanje novog sadrzaja na ekran*/
+	glutSwapBuffers();
 }
 
 static void on_keyboard(unsigned char key, int x, int y)
@@ -238,16 +268,41 @@ static void on_time(int value){
 		glutTimerFunc(0, on_time, 0);
 }
 
-static void on_reshape(int width, int height)
-{
-	/* Pamte se sirina i visina prozora. */
-	window_width = width;
-	window_height = height;
+/* Iscrtava nisan */
+void drawPointer() 
+{  
+	glMatrixMode(GL_PROJECTION); 
+	glPushMatrix();  
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW); 
+	glPushMatrix(); 
+	glLoadIdentity();
 	
-	/* Podesava se viewport. */
-	glViewport(0, 0, window_width, window_height);
+	glDisable(GL_LIGHT0);
+	glDisable(GL_LIGHTING);
+	
+	glColor3f(1, 0, 0);
+	gluOrtho2D(0.0, window_width, window_height, 0.0);
+	char display_string[20];
+	int words = sprintf(display_string,"%s", "-O-");
+	if(words < 0)
+		exit(1);
+	glRasterPos2i(window_width/2, window_height/2); 
+	int d = (int) strlen(display_string);
+	for (int i = 0; i < d; i++)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, display_string[i]);
+	
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	
+	glMatrixMode(GL_PROJECTION); 
+	glPopMatrix(); 
+	glMatrixMode(GL_MODELVIEW); 
+	glPopMatrix(); 
+	glutPostRedisplay();
 }
 
+/* Ispisuje vreme */
 void drawBitmapText() 
 {  
 	glMatrixMode(GL_PROJECTION); 
@@ -266,11 +321,11 @@ void drawBitmapText()
 	int words = sprintf(display_string,"%s", "Time:");
 	if(words < 0)
 		exit(1);
-	glRasterPos2i(window_width/2-30, window_height/2); 
+	glRasterPos2i(window_width/2-5, window_height/2 - 50); 
 	int d = (int) strlen(display_string);
 	for (int i = 0; i < d; i++)
 		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, display_string[i]);
-	glRasterPos2i(window_width/2 - 25, window_height/2+15); 
+	glRasterPos2i(window_width/2-5, window_height/2-30); 
 	int l = (int) strlen(disp_time);
 	for (int i = 0; i < l; i++)
 		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, disp_time[i]);
@@ -285,7 +340,7 @@ void drawBitmapText()
 	glutPostRedisplay();
 }
 
-
+/* Ispisuje poene */
 void drawScore(){
 	
 	glMatrixMode(GL_PROJECTION); 
@@ -308,12 +363,12 @@ void drawScore(){
 	
 	if(words < 0)
 		exit(1);
-	glRasterPos2i(window_width/2-50, window_height/2-70); 
+	glRasterPos2i(window_width/2-20, window_height/2+40); 
 	int d = (int) strlen(display_string);
 	for (int i = 0; i < d; i++)
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, display_string[i]);
 	
-	glRasterPos2i(window_width/2-20, window_height/2-40); 
+	glRasterPos2i(window_width/2+5, window_height/2+70); 
 	int l = (int) strlen(disp_score);
 	for (int i = 0; i < l; i++)
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, disp_score[i]);
@@ -328,64 +383,6 @@ void drawScore(){
 	glutPostRedisplay();
 }
 
-static void on_display(void)
-{
-	/* Brise se prethodni sadrzaj prozora. */
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	
-	/* Podesava se projekcija. */
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(
-		60,
-		1,
-		0.0,
-		10.0
-	);
-	/* Namestanje tacke pogleda kamere*/
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(x, 1.0f, z, 
-		  x+kx, 1.0f+ky, z+kz,
-	   0.0f, 1.0f, 0.0f
-	);
-	
-	init_lights();
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_COLOR_MATERIAL);
-	
-	
-	glDisable(GL_DEPTH_TEST);
-	
-	draw_coordinate_system();
-	draw_box();
-	
-	cube();
-	
-	x_ball = x;
-	y_ball =y;
-	z_ball = z;
-	
-	shoot();
-	
-	int score1  = sprintf(disp_score,"%d", score);
-	if(score1 < 0)
-		exit(1);
-	
-	/* Ispisivanje vremena */
-	drawBitmapText();
-	
-	/* Ispisivanje rezultata*/
-	if(!timer_active && ind == 1){
-		drawScore();
-	}
-	
-	glutPostRedisplay();
-	/* Slanje novog sadrzaja na ekran*/
-	glutSwapBuffers();
-}
-
 /* Postavlja slucajnu poziciju kocke */
 void cubepositions (void) {
 	
@@ -397,6 +394,7 @@ void cubepositions (void) {
 	}
 }
 
+/* Iscrtava kocke na slucajnim mestima */
 void cube (void) {
 	
 	for (int i=0;i<24;i++)
@@ -404,7 +402,7 @@ void cube (void) {
 		
 		glPushMatrix();
 		glColor3f(0.4, 1, 0.6);
-		glTranslated(positionx[i + 1], positiony[i + 1], positionz[i + 1]);
+		glTranslated(positionx[i + 1]+0.5, positiony[i + 1]+0.5, positionz[i + 1]+0.5);
 		glutSolidCube(0.5);
 		glPopMatrix();
 	}
@@ -444,7 +442,7 @@ static void draw_box()
 	glEnd();
 	
 	/* Iscrtavanje desnog zida */
-	glColor3f(0.4, 0.2, 0.7);
+	glColor3f(0.3, 0.2, 0.7);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glBegin(GL_POLYGON);
 	glVertex3f(10, 0, 0);
@@ -467,7 +465,6 @@ static void draw_box()
 
 static void draw_coordinate_system()
 {
-	
 	glBegin(GL_LINES);
 	glColor3f(1, 0, 0);
 	glVertex3f(-5, 0, 0);
@@ -482,19 +479,6 @@ static void draw_coordinate_system()
 	glVertex3f(0, 0, 100);
 	
 	glEnd();
-}
-
-void shoot(){
-	glPushMatrix();
-	glColor3f(1, 1, 1);
-	
-	x_ball = x_ball+t*bx*v;
-	y_ball = y_ball+0.9f+t*by*v; 
-	z_ball = z_ball+t*bz*v;
-	
-	glTranslatef(x_ball, y_ball, z_ball);
-	glutSolidSphere(radius, 30, 30);
-	glPopMatrix();
 }
 
 static void on_mouse(int button, int state, int x, int y){
@@ -517,7 +501,23 @@ static void on_mouse(int button, int state, int x, int y){
 			break;
 	}
 }
+
+/* Ispaljivanje kuglice */
+void shoot(){
+	glPushMatrix();
+	glColor3f(1, 1, 1);
+	
+	x_ball = x_ball+t*bx*v;
+	y_ball = y_ball+0.9f+t*by*v; 
+	z_ball = z_ball+t*bz*v;
+	
+	glTranslatef(x_ball, y_ball, z_ball);
+	glutSolidSphere(radius, 30, 30);
+	glPopMatrix();
+}
+
 void moving_ball(int value){
+	/* Proveravamo da li bas ovaj tajmer */
 	if (value != 1)
 		return;
 	
@@ -526,62 +526,128 @@ void moving_ball(int value){
 		y_ball > 6.0f || y_ball < 0.0f  || 
 		z_ball > 10.0f || z_ball < 0.0f){
 		move_ball = 0;
-		t = 0;
+	t = 0;
 	return;        
 		}
+	
+	/* Ako je kuglica pogodila kocku*/
+	int i;
+	for(i = 0; i < 25; i++){
+	if(z_ball  <= positionz[i] + 0.5 && z_ball >= positionz[i] - 0.5
+		&& y_ball <= positiony[i] + 0.5 && y_ball >= positiony[i] - 0.5
+		&& x_ball >= positionx[i] - 0.5 && x_ball <= positionx[i] + 0.5){
 		
-		/* Ako je kuglica pogodila kocku nestaje kocka*/
-		int i;
-		for(i = 0; i < 25; i++){
-			if(z_ball  <= positionz[i] + 0.5 && z_ball >= positionz[i] - 0.5
-				&& y_ball <= positiony[i] + 0.5 && y_ball >= positiony[i] - 0.5
-				&& x_ball >= positionx[i] - 0.5 && x_ball <= positionx[i] + 0.5){
-				
-				/* Kocka se generise na drugom mestu */
-				positionx[i] = rand()/(float)RAND_MAX*9;
-				positiony[i] = rand()/(float)RAND_MAX*4;
-				positionz[i] = rand()/(float)RAND_MAX*5;
-				score += 1;
-				}
+		/* Kocka se generise na drugom mestu */
+		positionx[i] = rand()/(float)RAND_MAX*9;
+		positiony[i] = rand()/(float)RAND_MAX*4;
+		positionz[i] = rand()/(float)RAND_MAX*5;
+		score += 1;
+	
+		/* Ako je kuglica pogodila jednu kocku, nestaje*/
+		x_ball = 100;
+		y_ball = 100;
+		z_ball = 100;
 			
 		}
-		
-		t += .2f;
-		if (move_ball)
-			glutTimerFunc(30, moving_ball, value);
+	}
+	
+	t += .2f;
+	if (move_ball)
+		glutTimerFunc(30, moving_ball, value);
 }
 
 static void on_mouse_motion(int x, int y){
 	
-	glutSetCursor(GLUT_CURSOR_FULL_CROSSHAIR);
-	// Calculate the offset movement between the last and current
-	// frame
-	GLfloat xoffset = x - lastX;
-	GLfloat yoffset = lastY - y; // reversed since y-coordinates range from bottom to top
-	
-	lastX = x;
-	lastY = y;
-	
-	xoffset *= sensitivity;
-	yoffset *= sensitivity;
-	
-	angleX += yoffset;
-	angleY += xoffset;
-	
-	if(angleX > 45.0f)
-		angleX = 45.0f;
-	if(angleX < -20.0f)
-		angleX = -20.0f;
-	if(angleY > -45.0f)
-		angleY = -45.0f;
-	if(angleY < -135.0f)
-		angleY = -135.0f;
-	
-	kx = cos(M_PI/180.0*angleX)*cos(M_PI/180.0*angleY);
-	ky = sin(M_PI/180.0*angleX);
-	kz = cos(M_PI/180.0*angleX) * sin(M_PI/180.0*angleY);
+	/* Ako kursor izadje van prozora*/
+	if( x > window_width-2.0f){
+		glutWarpPointer(window_width/2, window_height/2);
+	}
+	else if (x < 2.0f){
+		glutWarpPointer(window_width/2, window_height/2);
+	}
+	else if (y > window_height-2.0f){
+		glutWarpPointer(window_width/2, window_height/2);
+	}
+	else if (y < 2.0f){ 
+		glutWarpPointer(window_width/2, window_height/2);
+	}
+	else {
+		
+		glutSetCursor(GLUT_CURSOR_NONE);
+		/* Racuna razliku u pozicijama misa izmedju dva frejma*/
+		GLfloat xoffset = x - lastX;
+		GLfloat yoffset = lastY - y;
+		
+		lastX = x;
+		lastY = y;
+		
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+		
+		angleX += yoffset;
+		angleY += xoffset;
+		
+		/* Pomeranje misa levo - desno*/
+		if(angleX > 45.0f)
+			angleX = 45.0f;
+		if(angleX < -20.0f)
+			angleX = -20.0f;
+		/*Pomeranje misa gore - dole*/
+		if(angleY > -45.0f)
+			angleY = -45.0f;
+		if(angleY < -135.0f)
+			angleY = -135.0f;
+		
+		/* Vektor pomeranja kamere */
+		kx = cos(M_PI/180.0*angleX)*cos(M_PI/180.0*angleY);
+		ky = sin(M_PI/180.0*angleX);
+		kz = cos(M_PI/180.0*angleX) * sin(M_PI/180.0*angleY);
+	}
 }
 
+/* Inicijalizacija pocetnih vrednosti parametara */
+void initialize()
+{
+	/* Ugao rotacije oko y ose za pravac kamere*/
+	angleY = 0.0;
+	/* Ugao rotacije oko x ose za pravac kamere*/
+	angleX = 0.0;
+	
+	/* Vektor koji predstavlja pravac kamere */
+	kx = 0.0f;
+	ky = 0.0f;
+	kz = -1.0f;
+	
+	/* Pozicija kamere */
+	x = 5.0f;
+	y = 0.0f;
+	z = 9.0f;
+	
+	/* Odbrojavanje do kraja igre */
+	game_over = 0;
+	game_time = 0;
+	/* Fleg koji odredjuje stanje tajmera*/
+	timer_active = 0;
+	
+	/* Tajmer za ispucavanje kuglice */
+	t = 0;
+	move_ball = 0;
+	/* Vektor pravca kuglice */
+	bx = 0.0f;
+	by = 0.0f;
+	bz = -1.0f;
+	/* brzina kuglice */
+	v = 7.0f;
+	
+	/* Indikator da li je tajmer bio aktivan */
+	ind = 0;
+	
+	/* Broj osvojenih bodova */
+	score = 0;
+	
+}
+
+/* Inicijalizacija svetla */
 void init_lights(){
 	/* Pozicija svetla (u pitanju je direkcionalno svetlo). */
 	GLfloat light_position[] = { 0.5, 0.2, 0.5, 1 };
